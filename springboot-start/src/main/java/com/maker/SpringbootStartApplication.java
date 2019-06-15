7840137989d2f54c339c1ae64f2b1c91a3b26b0b
fantasticmaker <1763932166@qq.com>
2019-06-15 09:02:45 +0800

    no message

--- springboot-start/src/main/java/com/maker/SpringbootStartApplication.java
+++ springboot-start/src/main/java/com/maker/SpringbootStartApplication.java
@@ -11,7 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 //扫描 mybatis mapper 包路径：进行映射
 @MapperScan(basePackages = "com.maker.mapper")
 //扫描 所有需要的包, 包含一些自用的工具类包 所在的路径:扫描之后才可以进行依赖注入
-@ComponentScan(basePackages= {"com.maker"})
+//@ComponentScan(basePackages= {"com.maker"})//该路径为默认，可以不配置
 //开启定时任务:会自动扫描
 @EnableScheduling
 //开启异步调用方法:与@Async标签配合使用
